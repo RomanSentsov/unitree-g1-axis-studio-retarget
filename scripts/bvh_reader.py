b1d591b2e1d0cdb84806733871293eac84c151ad
RomanSentsov <rkey695@gmail.com>
2026-09-15 13:18:58 +0300

    added correct hand orientation

--- scripts/bvh_reader.py
+++ scripts/bvh_reader.py
@@ -5,6 +5,8 @@ import pinocchio as pin
 import numpy as np
 import time
 
+from scipy.spatial.transform import Rotation as R
+
 # ------------------------------ BVH OPEN ------------------------------
 
 FILEPATH = "recs/bvh_recs/bvh_misha_no_xyz_chr01_MAYA.bvh"
@@ -16,41 +18,64 @@ bvh = bvh.rotate_vertical(np.pi / 2)
 # bvh.play()
 
 # print(bvh.joint_names)
+# exit()
 
 
-# RightHand, LeftHand
+# get indexes of joints
 r_hand_idx = bvh.node_index["RightHand"]
 l_hand_idx = bvh.node_index["LeftHand"]
 
-r_hand_pos = bvh.node_positions(centered="skeleton")
-l_hand_pos = bvh.node_positions(centered="skeleton")
+r_mid_idx = bvh.node_index["RightHandMiddle1"]
+l_mid_idx = bvh.node_index["LeftHandMiddle1"]
 
-print(r_hand_pos.shape)
+r_pinky_idx = bvh.node_index["RightHandPinky1"]
+l_pinky_idx = bvh.node_index["LeftHandPinky1"]
 
-r_hand_pos = r_hand_pos[::3, :, :]
-l_hand_pos = l_hand_pos[::3, :, :]
+# Get positions
 
-print(r_hand_pos.shape)
+poses = bvh.node_positions(centered="skeleton")
 
-frame_num = r_hand_pos.shape[0]
+print(poses.shape)
 
+poses = np.array(poses[::3, :, :])
+
+print(poses.shape)
+
+frame_num = poses.shape[0]
+
+# Relative poses of finger fragments representig hand orient
+
+r_hand_x = poses[:, r_mid_idx] - poses[:, r_hand_idx]
+r_hand_y = poses[:, r_pinky_idx] - poses[:, r_mid_idx]
+
+l_hand_x = poses[:, l_mid_idx] - poses[:, l_hand_idx]
+l_hand_y = poses[:, l_pinky_idx] - poses[:, l_mid_idx]
+
+quat_list = [R.align_vectors(np.stack([r_hand_x[i], r_hand_y[i]], axis=0), 
+                              np.array([[1, 0, 0], [0, 0, -1]]))[0].as_quat() 
+             for i in range(len(r_hand_x))]
+r_quat = np.array(quat_list)
+
+quat_list = [R.align_vectors(np.stack([l_hand_x[i], l_hand_y[i]], axis=0), 
+                              np.array([[1, 0, 0], [0, 0, -1]]))[0].as_quat() 
+             for i in range(len(l_hand_x))]
+l_quat = np.array(quat_list)
 # ------------------------------ Play BVH via retarget  ------------------------------
 
 arm_ik = G1_29_ArmIK(Unit_Test = True, Visualization = True)
 
     # initial positon
-L_prev_target = pin.SE3(
-        pin.Quaternion(1, 0, 0, 0),
-        np.array(l_hand_pos[0, l_hand_idx])/200,
-    )
-
 R_prev_target = pin.SE3(
-        pin.Quaternion(1, 0, 0, 0),
-        np.array(r_hand_pos[0, r_hand_idx])/200,
+        pin.Quaternion(r_quat[0]),
+        poses[0, r_hand_idx]/200,
     )
 
-# bvh.play()
-# exit()
+L_prev_target = pin.SE3(
+        pin.Quaternion(l_quat[0]),
+        poses[0, l_hand_idx]/200,
+    )
+
+
 
 until = None
 now = None
@@ -58,16 +83,16 @@ now = None
 while True:
 
     for i in range(frame_num - 1):
+    #   for i in range(1):
+        R_tf_target = pin.SE3(
+            pin.Quaternion(r_quat[i]),
+            poses[i, r_hand_idx]/150,
+        )   
 
         L_tf_target = pin.SE3(
-            pin.Quaternion(1, 0, 0, 0),
-            np.array(l_hand_pos[i, l_hand_idx])/200,
+            pin.Quaternion(l_quat[i]),
+            poses[i, l_hand_idx]/150,
         )
-
-        R_tf_target = pin.SE3(
-            pin.Quaternion(1, 0, 0, 0),
-            np.array(r_hand_pos[i, r_hand_idx])/200,
-        )   
 
         until = time.time()
         arm_ik.solve_ik(L_tf_target.homogeneous, R_tf_target.homogeneous)
